9d24478d7caf9ecc495290eb8b229c2e4ed13695
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-05-26 01:12:16 +0300

    app

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 console.log(__dirname);
 module.exports = {
-  entry: "app.js",
+  entry: "./app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js",
