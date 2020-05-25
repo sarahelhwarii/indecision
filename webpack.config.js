67eaa061329aa98666f26227637e761f7c65992b
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-05-26 01:00:10 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+console.log(__dirname);
+module.exports = {
+  entry: "app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+};
